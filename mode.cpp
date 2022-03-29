570da52a92d6bb5961d12742ae425783895a67dd
billyz_astra <1651760758@qq.com>
2022-03-29 09:53:20 +0800

    main

---
+++ mode.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<limits.h>
+#include<queue>
+using namespace std;
+
+class Solution {
+public:
+
+};
+        
+void display(vector<int> x)
+{
+    for(int i = 0; i < x.size(); i++)
+    {
+        cout<<x[i]<<" ";
+    }
+    return ; 
+}
+
+int main(){
+    Solution x;
+    return 0 ;
+}
